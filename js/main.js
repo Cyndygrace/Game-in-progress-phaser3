3f6ab2f03ce8a3ddeb3730b1cd46ccdd20a3c7a8
cynthia iloekwe <cyniloekwe@gmail.com>
2020-06-10 09:23:28 +0100

    updated basic game config values to game values

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-// / is fired when the entire page loads, including its content
+// three types of graphic_mode,
+//  **Phaser.Canvas(this is a simple HTML 5 canvas)
+//  **Phaser.WebGL (web graphic library)
+//  **Phaser.AUTO (Allows phaser decide which mode to use when available)
+// is fired when the entire page loads, including its content
 window.onload = function () {
-  //basic game config to setup game canvas
-  var config = {
-    type: graphics_mode,
-    width: game_width,
-    height: game_height,
-    parent: 'div-tag-name',
-    scene: [nameOfScene],
+  //basic game config to setup game
+  let config = {
+    // choose graphic mode type
+    type: Phaser.Auto,
+    width: 480,
+    height: 640,
+    // id of the div in the html where the game will be displayed
+    parent: 'phaser-game',
+    // different game scenes like game over and start game, etc. in an array
+    scene: [SceneMain],
   };
 };
