7c50e52ef7a4a9b43055b2254e487762b3ec86d1
cynthia iloekwe <cyniloekwe@gmail.com>
2020-06-10 10:50:51 +0100

    added custom function with comments

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -1,12 +1,17 @@
 class SceneMain extends Phaser.Scene {
+  // constructor is called once the scene is created
   constructor() {
     // add the name of the scene inside super
     // the scene in the super function must match the class name
     super('SceneMain');
   }
+  // preload, loads all our resources(images, sounds, etc) before it is being fired
   preload() {}
+  // create defines our object
   create() {
     console.log('Ready!');
   }
+  // update, for any action we want to constantly update(eg running loop, collision, etc.)
   update() {}
+  customFunction() {}
 }
