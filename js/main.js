66035d663281fe2530c85a2f5616a1cf0796a629
cynthia iloekwe <cyniloekwe@gmail.com>
2020-06-10 09:13:08 +0100

    basic setup for game canvas from phaser3 starter config

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 // / is fired when the entire page loads, including its content
 window.onload = function () {
+  //basic game config to setup game canvas
+  var config = {
+    type: graphics_mode,
+    width: game_width,
+    height: game_height,
+    parent: 'div-tag-name',
+    scene: [nameOfScene],
+  };
 };
